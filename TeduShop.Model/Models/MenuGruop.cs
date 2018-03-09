e875cb92c657c2d3c70517f46aa4ad5d395920cb
quangtv-PC\quangtv <quangbusoco@gmail.com>
2018-03-09 14:40:33 +0700

    commit menu

---
+++ TeduShop.Model/Models/MenuGruop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("MenuGroups")]
+   public class MenuGruop
+    {
+        [Key]
+        public int ID { set; get; }
+        [Required]
+        public string Name { set; get; }
+        public virtual IEnumerable<Menu> Menus { set; get; }
+    }
+}
---
+++ TeduShop.Model/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Products")]
+   public class Product
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public string Alias { set; get; }
+
+    }
+}
