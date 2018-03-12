1a1a15663c2b6a48a67364d1b49d0ddebb9e5272
quangtv-PC\quangtv <quangbusoco@gmail.com>
2018-03-12 09:00:03 +0700

    comit products

--- TeduShop.Model/Models/Product.cs
+++ TeduShop.Model/Models/Product.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace TeduShop.Model.Models
 {
@@ -13,6 +14,19 @@ namespace TeduShop.Model.Models
         public int ID { set; get; }
         public string Name { set; get; }
         public string Alias { set; get; }
+        public string CategoryID { set; get; }
+        public string Image { set; get; }
+        public XElement MoreImages { set; get; }
+        public decimal Price { set; get; }
+        public decimal? PromotionPrice { set; get; }
+        public int? Warranty { set; get; }
+        public string Description { set; get; }
+        public string Conten { set; get; }
+        public bool HomeFlag { set; get; }
+        public bool HotFlag { set; get; }
+        public int ViewCoun { set; get; }
+        [ForeignKey("CategoryID")]
+        public virtual ProductCategory ProductCategory { set; get; }
 
     }
 }
---
+++ TeduShop.Model/Models/ProductCategorie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+   [Table("ProductCategories")]
+   public class ProductCategory
+    {
+        [Key]
+        public int ID { set; get;}
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Alias { set; get; }
+        public int ParentID { set; get; }
+        public  int DislayOder { set; get; }
+        public string Discriptint { set; get; }
+        public string Image { set; get; }
+        public string Metakeyword { set; get; }
+        public string matadiscription { set; get; }
+        public DateTime CreatedDate { set; get; }
+        public DateTime CreatedBy { set; get; }
+        public DateTime UpdatedDate { set; get; }
+        public string UpdatedBy { set; get; }
+        public bool Status { set; get; }
+        public bool HomeFlag { set; get; }
+        public virtual IEnumerable<Product> Products { set; get; }
+       
+    }   
+}
